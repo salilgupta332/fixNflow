e3e29a554751b7980c236647af27e48fa03af7eb
salilgupta332 <salilgupta332@gmail.com>
2025-09-07 18:56:06 +0530

    Files updated

--- fixNflow-backend/routes/authRoutes.js
+++ fixNflow-backend/routes/authRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser } = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 // Registration endpoint
 router.post('/register', registerUser);
 
 // Login endpoint
 router.post('/login', loginUser);
+
+router.get('/profile', authMiddleware, (req, res) => {
+  res.json({ message: `User ID: ${req.userId} authorized` });
+});
 
 module.exports = router;
--- fixNflow-backend/server.js
+++ fixNflow-backend/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-
+app.use('/api/auth', require('./routes/authRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
