8afe12273f87337cb010aabdfe1f88af7bed389b
salilgupta332 <salilgupta332@gmail.com>
2025-09-07 17:32:37 +0530

    Files Updated

--- fixNflow-backend/server.js
+++ fixNflow-backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const connectDB = require('./config/db');
-
 dotenv.config();
+
+const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', require('./routes/authRoutes'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
